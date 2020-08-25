5498a803ed48e688c6a5abc1a9704ea974b33e21
Mohib Ali <59516133+dev-mohib@users.noreply.github.com>
2020-08-26 00:14:18 +0500

    update

---
+++ assets/content.js
@@ -0,0 +1,79 @@
+const mainContent = [
+    {
+        title : "mStore Multi vendor E-commerce App",
+        category : "Mobile",
+        description : "This is an E-commerce Mobile App Available on Both Platforms android and iOS, This app is connected with Woocommerce through API to show the Products from Woocommerce store to this App. This project is not completely done by me but I have worked with team of developers to make this app",
+        fearures : [
+                    "Manage Woocomerce products",
+                    "Adding products to cart",
+                    "Paymeny Gatways Paypal, Stripe",
+                    "Chat with Vendor"
+    ],
+        images : ["assets/img/service/services1.png", "assets/img/service/services1.png"]
+    },
+    {
+        title : "Ride App - Mobile App for texi booking ",
+        category : "Mobile",
+        description : "This app is a texi booking app. with user app, driver app and admin panel, This is available on both platforms",
+        features : [
+                "Tracking user location and displaying available drivers arround the uer",
+                "Payment feature for Paying fares",
+                "Separate App for managing driver tasks",
+                "Driver withdraw functionality",
+                "Admin dashboard for the owner to manage the users, drivers and listen their feedbacks"
+        ],
+        images : ["assets/img/service/services1.png", "assets/img/service/services1.png"]
+    },
+    {
+        title : "LendMe360 - Finiacial Reports Website to create and view Reports",
+        category : "Web",
+        description : "This Website is an accounting wensite where user can create his reports",
+        features : [
+                "Tracking user location and displaying available drivers arround the uer",
+                "Payment feature for Paying fares",
+                "Separate App for managing driver tasks",
+                "Driver withdraw functionality",
+                "Admin dashboard for the owner to manage the users, drivers and listen their feedbacks"
+        ],
+        images : ["assets/img/service/services1.png", "assets/img/service/services1.png"]
+    },    {
+        title : "Mizaharsiv - Personal Private website for specific purpose",
+        category : "Web",
+        description : "This website is built for a client from france, This website is private website which is managed by a unique admin",
+        features : [
+                "Tracking user location and displaying available drivers arround the uer",
+                "Payment feature for Paying fares",
+                "Separate App for managing driver tasks",
+                "Driver withdraw functionality",
+                "Admin dashboard for the owner to manage the users, drivers and listen their feedbacks"
+        ],
+        images : ["assets/img/service/services1.png", "assets/img/service/services1.png"]
+    },
+    {
+        title : "Swissschoolofphotography",
+        category : "UX",
+        description : "This app is a texi booking app",
+        features : [
+                "Tracking user location and displaying available drivers arround the uer",
+                "Payment feature for Paying fares",
+                "Separate App for managing driver tasks",
+                "Driver withdraw functionality",
+                "Admin dashboard for the owner to manage the users, drivers and listen their feedbacks"
+        ],
+        images : ["assets/img/service/services1.png", "assets/img/service/services1.png"]
+    },
+    {
+        title : "coinify",
+        category : "Ux",
+        description : "This app is a texi booking app",
+        features : [
+                "Tracking user location and displaying available drivers arround the uer",
+                "Payment feature for Paying fares",
+                "Separate App for managing driver tasks",
+                "Driver withdraw functionality",
+                "Admin dashboard for the owner to manage the users, drivers and listen their feedbacks"
+        ],
+        images : ["assets/img/service/services1.png", "assets/img/service/services1.png"]
+    }
+
+]
---
+++ assets/script.js
@@ -0,0 +1,61 @@
+
+
+
+let mobileDOM = ``
+let webAppDOM = ``
+let uxDOM = ``
+mainContent.map((content, index) => {
+    switch(content.category) {
+        case 'Mobile':
+           mobileDOM += `<div class="row">
+                            <div class="col-lg-5 col-md-5 col-sm-5">
+                                <!-- Single -->
+                                <div class="single-services mb-30">
+                                    <img height="350" src="assets/img/service/services1.png" alt="">
+                                </div>
+                            </div>
+                            <div class="col-lg-7 col-md-7 col-sm-7">
+                                <h2 class="h2">${content.title}</h2>
+                                <p>${content.description}</p>
+                                <p><b>Link</b> <a href="#">Click here</a></p>
+                            </div>
+                        </div>`
+        break
+        case 'Web':
+            webAppDOM += `<div class="row">
+                            <div class="col-lg-5 col-md-5 col-sm-5">
+                                <!-- Single -->
+                                <div class="single-services mb-30">
+                                    <img height="350" src="assets/img/service/services1.png" alt="">
+                                </div>
+                            </div>
+                            <div class="col-lg-7 col-md-7 col-sm-7">
+                                <h2 class="h2">${content.title}</h2>
+                                <p>${content.description}</p>
+                                <p><b>Link</b> <a href="#">Click here</a></p>
+                            </div>
+                        </div>`
+        break
+        case 'UX':
+            uxDOM += `<div class="row">
+                        <div class="col-lg-5 col-md-5 col-sm-5">
+                            <!-- Single -->
+                            <div class="single-services mb-30">
+                                <img height="350" src="assets/img/service/services1.png" alt="">
+                            </div>
+                        </div>
+                        <div class="col-lg-7 col-md-7 col-sm-7">
+                            <h2 class="h2">${content.title}</h2>
+                            <p>${content.description}</p>
+                            <p><b>Link</b> <a href="#">Click here</a></p>
+                        </div>
+                    </div>`
+        break
+        default:
+            console.log("These projects are uncategorized")
+    }
+})
+
+document.getElementById('MobileApps').innerHTML = mobileDOM
+document.getElementById('WebApps').innerHTML = webAppDOM
+document.getElementById('UI-UX').innerHTML = uxDOM
